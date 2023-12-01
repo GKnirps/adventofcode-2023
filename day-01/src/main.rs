c40e8515bc9e8e5121a5fc92397f5e7b19153f5a
Guido Knips <guido.knips@posteo.de>
2023-12-01 17:10:33 +0100

    Add tests for day 1
    Not really needed, but just so I have them

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), String> {
         .ok_or_else(|| "No file name given.".to_owned())?;
     let content = read_to_string(Path::new(&filename)).map_err(|e| e.to_string())?;
 
-    let calibration_sum: u32 = content.lines().map(parse_line).sum();
+    let calibration_sum: u32 = content.lines().map(bad_calibration).sum();
     println!("The sum of the calibration values is {calibration_sum}");
 
     let correct_calibration_sum: u32 = content.lines().map(full_calibration).sum();
@@ -17,7 +17,7 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
-fn parse_line(line: &str) -> u32 {
+fn bad_calibration(line: &str) -> u32 {
     line.chars()
         .filter_map(|c| c.to_digit(10))
         .next()
@@ -77,4 +77,25 @@ fn full_calibration(line: &str) -> u32 {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn test_bad_calibration() {
+        assert_eq!(bad_calibration("1abc2"), 12);
+        assert_eq!(bad_calibration("pqr3stu8vwx"), 38);
+        assert_eq!(bad_calibration("a1b2c3d4e5f"), 15);
+        assert_eq!(bad_calibration("treb7uchet"), 77);
+    }
+
+    #[test]
+    fn test_full_calibration() {
+        assert_eq!(full_calibration("two1nine"), 29);
+        assert_eq!(full_calibration("eightwothree"), 83);
+        assert_eq!(full_calibration("abcone2threexyz"), 13);
+        assert_eq!(full_calibration("xtwone3four"), 24);
+        assert_eq!(full_calibration("4nineeightseven2"), 42);
+        assert_eq!(full_calibration("zoneight234"), 14);
+        assert_eq!(full_calibration("7pqrstsixteen"), 76);
+        assert_eq!(full_calibration("eighthree"), 83);
+        assert_eq!(full_calibration("sevenine"), 79);
+    }
 }
