1032b079edf624aab4ab37b172f5ccb2174ea729
Guido Knips <guido.knips@posteo.de>
2023-12-03 10:11:49 +0100

    Add solution for day 3, part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -7,14 +7,33 @@ fn main() -> Result<(), String> {
         .nth(1)
         .ok_or_else(|| "No file name given.".to_owned())?;
     let content = read(Path::new(&filename)).map_err(|e| e.to_string())?;
+    let (numbers, symbols) = parse(&content);
 
-    let part_numbers = part_numbers_sum(&content);
+    let part_numbers = part_numbers_sum(&numbers, &symbols);
     println!("The sum of the part numbers is {part_numbers}.");
+
+    let gears = gear_ratio_sum(&numbers, &symbols);
+    println!("The sum of gear ratios is {gears}.");
 
     Ok(())
 }
 
-fn part_numbers_sum(schematic: &[u8]) -> u32 {
+#[derive(Debug)]
+struct SchematicNumber {
+    value: u32,
+    line: isize,
+    first: isize,
+    length: isize,
+}
+
+#[derive(Debug)]
+struct Symbol {
+    s: u8,
+    line: isize,
+    col: isize,
+}
+
+fn parse(schematic: &[u8]) -> (Vec<SchematicNumber>, Vec<Symbol>) {
     // assume input is rectangular
     let width = schematic
         .iter()
@@ -25,67 +44,88 @@ fn part_numbers_sum(schematic: &[u8]) -> u32 {
         .unwrap_or(schematic.len());
     let height = schematic.len() / width;
 
-    let mut sum: u32 = 0;
-    let mut current: Option<u32> = None;
-    let mut is_part = false;
+    let mut numbers: Vec<SchematicNumber> = Vec::with_capacity(width * height / 3);
+    let mut symbols: Vec<Symbol> = Vec::with_capacity(width * height);
+    let mut current: Option<(u32, usize)> = None;
 
     for y in 0..height {
         for x in 0..width {
             let c = schematic[y * width + x];
             if c.is_ascii_digit() {
-                is_part = is_part || symbol_adjacent(schematic, width, height, x, y);
                 let digit = (c - b'0') as u32;
-                if let Some(number) = current {
-                    current = Some(number * 10 + digit);
+                if let Some((number, start)) = current {
+                    current = Some((number * 10 + digit, start));
                 } else {
-                    current = Some(digit);
+                    current = Some((digit, x));
                 }
-            } else if let Some(number) = current {
-                if is_part {
-                    sum += number;
+            } else {
+                if let Some((number, start)) = current {
+                    numbers.push(SchematicNumber {
+                        value: number,
+                        line: y as isize,
+                        first: start as isize,
+                        length: (x - start) as isize,
+                    });
+                    current = None;
                 }
-                current = None;
-                is_part = false;
+                if c != b'.' && c != b'\n' {
+                    symbols.push(Symbol {
+                        s: c,
+                        line: y as isize,
+                        col: x as isize,
+                    })
+                }
             }
         }
     }
-    sum
+    (numbers, symbols)
 }
 
-fn symbol_adjacent(schematic: &[u8], width: usize, height: usize, x: usize, y: usize) -> bool {
-    let x = x as isize;
-    let y = y as isize;
-    [
-        (x - 1, y - 1),
-        (x - 1, y),
-        (x - 1, y + 1),
-        (x, y - 1),
-        (x, y + 1),
-        (x + 1, y - 1),
-        (x + 1, y),
-        (x + 1, y + 1),
-    ]
-    .iter()
-    .filter_map(|(nx, ny)| get(schematic, width, height, *nx, *ny))
-    .any(|c| c != b'.' && !c.is_ascii_digit() && c != b'\n')
+fn part_numbers_sum(numbers: &[SchematicNumber], symbols: &[Symbol]) -> u32 {
+    numbers
+        .iter()
+        .filter(|number| {
+            symbols
+                .iter()
+                .any(|symbol| number_symbol_adjacent(number, symbol))
+        })
+        .map(|number| number.value)
+        .sum::<u32>()
 }
 
-fn get(schematic: &[u8], width: usize, height: usize, x: isize, y: isize) -> Option<u8> {
-    if y < 0 || x < 0 || y as usize >= height || x as usize >= width {
-        None
-    } else {
-        schematic.get(x as usize + y as usize * width).copied()
-    }
+fn number_symbol_adjacent(number: &SchematicNumber, symbol: &Symbol) -> bool {
+    (symbol.line == number.line
+        && (symbol.col == number.first - 1 || symbol.col == number.first + number.length))
+        || ((symbol.line == number.line - 1 || symbol.line == number.line + 1)
+            && symbol.col >= number.first - 1
+            && symbol.col <= number.first + number.length)
+}
+
+fn gear_ratio_sum(numbers: &[SchematicNumber], symbols: &[Symbol]) -> u32 {
+    symbols
+        .iter()
+        .filter(|symbol| symbol.s == b'*')
+        .filter_map(|symbol| {
+            let (n, gear_ratio) = numbers
+                .iter()
+                .filter(|number| number_symbol_adjacent(number, symbol))
+                .fold((0usize, 1u32), |(n, gear_ratio), number| {
+                    (n + 1, number.value * gear_ratio)
+                });
+            if n == 2 {
+                Some(gear_ratio)
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[test]
-    fn part_number_sum_work_for_example() {
-        // given
-        let schematic = br#"467..114..
+    const SCHEMATIC: &[u8] = br#"467..114..
 ...*......
 ..35..633.
 ......#...
@@ -97,10 +137,27 @@ mod test {
 .664.598..
 "#;
 
+    #[test]
+    fn part_number_sum_works_for_example() {
+        // given
+        let (numbers, symbols) = parse(SCHEMATIC);
+
         // when
-        let result = part_numbers_sum(schematic);
+        let result = part_numbers_sum(&numbers, &symbols);
 
         // then
         assert_eq!(result, 4361);
+    }
+
+    #[test]
+    fn gear_ratio_sum_works_for_example() {
+        // given
+        let (numbers, symbols) = parse(SCHEMATIC);
+
+        // when
+        let result = gear_ratio_sum(&numbers, &symbols);
+
+        // then
+        assert_eq!(result, 467835);
     }
 }
