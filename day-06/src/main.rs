32993d96083de08c24a6d9a41d4dd221a58570ef
Guido Knips <guido.knips@posteo.de>
2023-12-06 07:36:17 +0100

    Add solution for day 6, part 2
    - brute force solution (because it works)
    - I have an idea for the better solution, but breakfast first

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -12,10 +12,19 @@ fn main() -> Result<(), String> {
     let beat_record_prod = ways_to_win(&races);
     println!("The product of the number of ways to win the race is {beat_record_prod}");
 
+    let race = fix_bad_kerning(&races);
+    // there is probably a smarter way to do this
+    // e.g. using simple math to find the first and the last winning combination
+    // and calculating the number of possibilities from there
+    // with the `--release` flag, the brute force solution only takes about 125ms, so I'll skip the
+    // optimization for now.
+    let beat_record_long_race = ways_to_win(&[race]);
+    println!("When there is only one long race, the product is {beat_record_long_race}");
+
     Ok(())
 }
 
-fn parse(input: &str) -> Result<Vec<(u32, u32)>, String> {
+fn parse(input: &str) -> Result<Vec<(u64, u64)>, String> {
     let (time, distance) = input
         .split_once('\n')
         .ok_or_else(|| "expected two lines in the input".to_owned())?;
@@ -24,7 +33,7 @@ fn parse(input: &str) -> Result<Vec<(u32, u32)>, String> {
         .ok_or_else(|| format!("prefix 'Time:' missing in line '{time}'"))?
         .split_whitespace()
         .map(|s| {
-            s.parse::<u32>()
+            s.parse::<u64>()
                 .map_err(|e| format!("unable to parse number '{s}': {e}"))
         });
     let distances = distance
@@ -32,14 +41,23 @@ fn parse(input: &str) -> Result<Vec<(u32, u32)>, String> {
         .ok_or_else(|| format!("prefix 'Distance:' missing in line '{distance}'"))?
         .split_whitespace()
         .map(|s| {
-            s.parse::<u32>()
+            s.parse::<u64>()
                 .map_err(|e| format!("unable to parse number '{s}': {e}"))
         });
 
     times.zip(distances).map(|(t, d)| Ok((t?, d?))).collect()
 }
 
-fn ways_to_win(races: &[(u32, u32)]) -> usize {
+fn fix_bad_kerning(races: &[(u64, u64)]) -> (u64, u64) {
+    races.iter().fold((0, 0), |(t, d), (part_t, part_d)| {
+        (
+            t * 10u64.pow(part_t.ilog10() + 1) + part_t,
+            d * 10u64.pow(part_d.ilog10() + 1) + part_d,
+        )
+    })
+}
+
+fn ways_to_win(races: &[(u64, u64)]) -> usize {
     // for now, a simple brute force approach will do (too early in the morning).
     // I'll probably regret this in part 2
     races
@@ -71,5 +89,18 @@ Distance:  9  40  200
 
         // then
         assert_eq!(result, 288);
+    }
+
+    #[test]
+    fn fix_bad_kerning_works_for_example() {
+        // given
+        let races = parse(RACES).expect("expected successful parsing");
+
+        // when
+        let (t, d) = fix_bad_kerning(&races);
+
+        // then
+        assert_eq!(t, 71530);
+        assert_eq!(d, 940200);
     }
 }
