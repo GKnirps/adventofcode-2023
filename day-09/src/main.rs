0a689e7aeca5e120df83ce2442e9c84fe81c1861
Guido Knips <guido.knips@posteo.de>
2023-12-09 13:38:29 +0100

    Add solution for day 9, part 2
    That was easier than expected. I didn't even have to improve the
    performance. And that on a weekend!

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -9,8 +9,9 @@ fn main() -> Result<(), String> {
     let content = read_to_string(Path::new(&filename)).map_err(|e| e.to_string())?;
     let values = parse_values(&content)?;
 
-    if let Some(prediction) = prediction_sum(&values) {
-        println!("The sum of predicted values is {prediction}");
+    if let Some((pred_prev, pred_next)) = prediction_sum(&values) {
+        println!("The sum of predicted next values is {pred_next}");
+        println!("The sum of predicted previous values is {pred_prev}");
     } else {
         println!("It's unpredicatable…");
     }
@@ -31,7 +32,7 @@ fn parse_values(input: &str) -> Result<Vec<Vec<i64>>, String> {
     input.lines().map(parse_line).collect()
 }
 
-fn predict(values: &[i64]) -> Option<i64> {
+fn predict(values: &[i64]) -> Option<(i64, i64)> {
     // let's do a primitive approach first
     let mut differences: Vec<Vec<i64>> = Vec::with_capacity(values.len());
     let mut diff_values: &[i64] = values;
@@ -40,18 +41,26 @@ fn predict(values: &[i64]) -> Option<i64> {
         diff_values = &differences[differences.len() - 1];
     }
 
-    let mut diff: i64 = 0;
+    let mut first_diff: i64 = 0;
+    let mut last_diff: i64 = 0;
     for diffs in differences.iter().rev() {
-        diff += diffs.last()?;
+        first_diff = diffs.first()? - first_diff;
+        last_diff += diffs.last()?;
     }
-    values.last().map(|v| v + diff)
+    let pred_prev = values.first().map(|v| v - first_diff)?;
+    let pred_next = values.last().map(|v| v + last_diff)?;
+
+    Some((pred_prev, pred_next))
 }
 
-fn prediction_sum(values: &[Vec<i64>]) -> Option<i64> {
+fn prediction_sum(values: &[Vec<i64>]) -> Option<(i64, i64)> {
     values
         .iter()
         .map(|v| predict(v))
-        .try_fold(0, |sum, next| Some(sum + next?))
+        .try_fold((0, 0), |(sum_p, sum_n), next| {
+            let (p, n) = next?;
+            Some((sum_p + p, sum_n + n))
+        })
 }
 
 #[cfg(test)]
@@ -65,9 +74,9 @@ mod test {
 
     #[test]
     fn predict_works_for_example() {
-        assert_eq!(predict(&[0, 3, 6, 9, 12, 15]), Some(18));
-        assert_eq!(predict(&[1, 3, 6, 10, 15, 21]), Some(28));
-        assert_eq!(predict(&[10, 13, 16, 21, 30, 45]), Some(68));
+        assert_eq!(predict(&[0, 3, 6, 9, 12, 15]), Some((-3, 18)));
+        assert_eq!(predict(&[1, 3, 6, 10, 15, 21]), Some((0, 28)));
+        assert_eq!(predict(&[10, 13, 16, 21, 30, 45]), Some((5, 68)));
     }
 
     #[test]
@@ -79,6 +88,6 @@ mod test {
         let sum = prediction_sum(&values);
 
         // then
-        assert_eq!(sum, Some(114));
+        assert_eq!(sum, Some((2, 114)));
     }
 }
