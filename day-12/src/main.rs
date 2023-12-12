c01e5cc5a6ce0f130c840b3b505847099af07b17
Guido Knips <guido.knips@posteo.de>
2023-12-12 11:38:28 +0100

    Add solution for day 12, part 2
    I need an algorithm to figure out in how many ways my brain was not
    operational today
    
    clippy is still complaining, but I don't care anymore
    
    It's 23:36, don't believe the git timestamp, that is due to squashing

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::read_to_string;
+use std::iter::once;
 use std::path::Path;
 
 fn main() -> Result<(), String> {
@@ -13,20 +15,65 @@ fn main() -> Result<(), String> {
     let arrangements = sum_arrangements(&rows);
     println!("The sum of the possible arrangements of all rows is {arrangements}");
 
+    let unfolded_rows = unfold_rows(&rows);
+    let arrangements = sum_arrangements(&unfolded_rows);
+    println!("The sum of the possible arrangements of all unfolded rows is {arrangements}");
+
     Ok(())
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 enum Spring {
     Op,
     Dmg,
     Unk,
 }
 
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
 struct Row {
     spring_conditions: Vec<(Spring, usize)>,
     damaged_groups: Vec<usize>,
+}
+
+fn unfold_rows(rows: &[Row]) -> Vec<Row> {
+    rows.iter().map(unfold_row).collect()
+}
+
+fn unfold_row(row: &Row) -> Row {
+    let spring_conditions: Vec<(Spring, usize)> = row
+        .spring_conditions
+        .iter()
+        .copied()
+        .chain(once((Spring::Unk, 1)))
+        .cycle()
+        .take(5 * row.spring_conditions.len() + 4)
+        .fold(
+            Vec::with_capacity(5 * row.spring_conditions.len() + 4),
+            |mut all, cond| {
+                if all.is_empty() {
+                    all.push(cond);
+                } else {
+                    let last = all.len() - 1;
+                    if cond.0 == all[last].0 {
+                        all[last].1 += cond.1;
+                    } else {
+                        all.push(cond);
+                    }
+                }
+                all
+            },
+        );
+    let damaged_groups = row
+        .damaged_groups
+        .iter()
+        .copied()
+        .cycle()
+        .take(5 * row.damaged_groups.len())
+        .collect();
+    Row {
+        spring_conditions,
+        damaged_groups,
+    }
 }
 
 fn parse(input: &str) -> Result<Vec<Row>, String> {
@@ -79,97 +126,157 @@ fn parse_row(line: &str) -> Result<Row, String> {
     })
 }
 
-fn sum_arrangements(rows: &[Row]) -> usize {
+fn sum_arrangements(rows: &[Row]) -> u128 {
     rows.iter().map(count_arrangements).sum()
 }
 
-fn count_arrangements(row: &Row) -> usize {
-    // Again, I have no good idea how to approach this, so I start with some brute forcing
-    let unknown_count = row
-        .spring_conditions
-        .iter()
-        .map(
-            |(cond, count)| {
-                if *cond == Spring::Unk {
-                    *count
-                } else {
-                    0
-                }
-            },
-        )
-        .sum::<usize>();
-    let damaged_count = row
-        .spring_conditions
-        .iter()
-        .map(
-            |(cond, count)| {
-                if *cond == Spring::Dmg {
-                    *count
-                } else {
-                    0
-                }
-            },
-        )
-        .sum::<usize>();
-    let required_damaged_count = row.damaged_groups.iter().sum::<usize>();
-
-    if damaged_count > required_damaged_count {
-        return 0;
-    }
-
-    (0..2u64.pow(unknown_count as u32))
-        .filter(|pattern| arrangement_matches(row, *pattern))
-        .count()
+type CountCache = HashMap<(Vec<(Spring, usize)>, Vec<usize>), u128>;
+fn count_arrangements(row: &Row) -> u128 {
+    let mut cache: CountCache = HashMap::with_capacity(4096);
+    count_recursive_cached(&mut cache, &row.spring_conditions, &row.damaged_groups)
 }
 
-// this is overly complicated, but my brain does not work right now
-fn arrangement_matches(row: &Row, mut pattern: u64) -> bool {
-    let mut springs_i = 0;
-    let mut in_unknown_i = 0;
-    for group in &row.damaged_groups {
-        let mut damage_count: usize = 0;
-        'springs: while springs_i < row.spring_conditions.len() {
-            match row.spring_conditions[springs_i].0 {
-                Spring::Op => {
-                    if damage_count > 0 {
-                        break;
-                    }
-                    springs_i += 1;
-                    continue;
-                }
+fn count_recursive_cached(
+    cache: &mut CountCache,
+    springs: &[(Spring, usize)],
+    groups: &[usize],
+) -> u128 {
+    // I could do some stuff to minimize allocation here (or around this), but seriously, I am out
+    // of energy
+    if let Some(count) = cache.get(&(springs.to_vec(), groups.to_vec())) {
+        *count
+    } else {
+        let count = count_recursive(cache, springs, groups);
+        cache.insert((springs.to_vec(), groups.to_vec()), count);
+        count
+    }
+}
+
+fn count_recursive(cache: &mut CountCache, springs: &[(Spring, usize)], groups: &[usize]) -> u128 {
+    if let Some(mut group) = groups.first().copied() {
+        let mut spring_i = 0;
+        while spring_i < springs.len() {
+            let len = springs[spring_i].1;
+            match springs[spring_i].0 {
+                Spring::Op => {}
                 Spring::Dmg => {
-                    damage_count += row.spring_conditions[springs_i].1;
+                    return if len > group {
+                        0
+                    } else {
+                        group -= len;
+                        let mut configs = 0;
+                        while spring_i < springs.len() {
+                            spring_i += 1;
+                            if let Some(next_spring) = springs.get(spring_i) {
+                                match next_spring.0 {
+                                    Spring::Op => {
+                                        configs = if group == 0 {
+                                            count_recursive_cached(
+                                                cache,
+                                                &springs[spring_i + 1..],
+                                                &groups[1..],
+                                            )
+                                        } else {
+                                            0
+                                        };
+                                        break;
+                                    }
+                                    Spring::Dmg => {
+                                        if next_spring.1 > group {
+                                            configs = 0;
+                                            break;
+                                        } else {
+                                            group -= next_spring.1;
+                                        }
+                                    }
+                                    Spring::Unk => {
+                                        if next_spring.1 == group {
+                                            group = 0;
+                                        } else if next_spring.1 < group {
+                                            group -= next_spring.1;
+                                        } else if next_spring.1 > group + 1 {
+                                            let mut subsprings = springs[spring_i..].to_vec();
+                                            subsprings[0].1 -= group + 1;
+                                            configs = count_recursive_cached(
+                                                cache,
+                                                &subsprings,
+                                                &groups[1..],
+                                            );
+                                            break;
+                                        } else {
+                                            configs = count_recursive_cached(
+                                                cache,
+                                                &springs[spring_i + 1..],
+                                                &groups[1..],
+                                            );
+                                            break;
+                                        }
+                                    }
+                                }
+                            } else {
+                                configs = (group == 0 && groups.len() < 2).into();
+                            }
+                        }
+                        configs
+                    }
                 }
                 Spring::Unk => {
-                    for i in in_unknown_i..row.spring_conditions[springs_i].1 {
-                        let operational = pattern & 1 == 0;
-                        pattern = pattern >> 1;
-                        in_unknown_i = i + 1;
-                        if operational {
-                            if damage_count > 0 {
-                                break 'springs;
+                    let mut configs: u128 = 0;
+                    for dmg_start in 0..len {
+                        for dmg_len in 1..=(len - dmg_start) {
+                            if dmg_start + dmg_len == len {
+                                if dmg_len == group {
+                                    if let Some(next_spring) = springs.get(spring_i + 1) {
+                                        if next_spring.0 == Spring::Op {
+                                            configs += count_recursive_cached(
+                                                cache,
+                                                &springs[spring_i + 1..],
+                                                &groups[1..],
+                                            );
+                                        }
+                                    } else {
+                                        configs += if groups.len() < 2 { 1 } else { 0 };
+                                    }
+                                } else if dmg_len < group {
+                                    if let Some(next_spring) = springs.get(spring_i + 1) {
+                                        if next_spring.0 == Spring::Dmg {
+                                            let mut subgroups = groups.to_vec();
+                                            subgroups[0] -= dmg_len;
+                                            configs += count_recursive_cached(
+                                                cache,
+                                                &springs[spring_i + 1..],
+                                                &subgroups,
+                                            );
+                                        }
+                                    }
+                                }
+                            } else if dmg_len == group {
+                                if len == dmg_start + dmg_len + 1 {
+                                    configs += count_recursive_cached(
+                                        cache,
+                                        &springs[spring_i + 1..],
+                                        &groups[1..],
+                                    );
+                                } else {
+                                    let mut subsprings = springs[spring_i..].to_vec();
+                                    subsprings[0].1 -= dmg_start + dmg_len + 1;
+                                    configs +=
+                                        count_recursive_cached(cache, &subsprings, &groups[1..]);
+                                }
                             }
-                            continue;
-                        } else {
-                            damage_count += 1;
                         }
                     }
-                    in_unknown_i = 0;
+                    // also count options when all unknown springs where counted as operational
+                    return configs
+                        + count_recursive_cached(cache, &springs[spring_i + 1..], groups);
                 }
             }
-            springs_i += 1;
+            spring_i += 1;
         }
-
-        if *group != damage_count {
-            return false;
-        }
+        0
+    } else {
+        springs.iter().all(|spring| spring.0 != Spring::Dmg).into()
     }
-    pattern == 0
-        && !row
-            .spring_conditions
-            .iter()
-            .skip(springs_i)
-            .any(|(condition, _)| *condition == Spring::Dmg)
 }
 
 #[cfg(test)]
@@ -219,6 +326,52 @@ mod test {
                 &parse_row("?###???????? 3,2,1").expect("expected successful parsing")
             ),
             10
+        );
+        assert_eq!(
+            count_arrangements(&unfold_row(
+                &parse_row("???.### 1,1,3").expect("expected successful parsing")
+            )),
+            1
+        );
+        assert_eq!(
+            count_arrangements(&unfold_row(
+                &parse_row(".??..??...?##. 1,1,3").expect("expected successful parsing")
+            )),
+            16384
+        );
+        assert_eq!(
+            count_arrangements(&unfold_row(
+                &parse_row("?#?#?#?#?#?#?#? 1,3,1,6").expect("expected successful parsing")
+            )),
+            1
+        );
+        assert_eq!(
+            count_arrangements(&unfold_row(
+                &parse_row("????.#...#... 4,1,1").expect("expected successful parsing")
+            )),
+            16
+        );
+        assert_eq!(
+            count_arrangements(&unfold_row(
+                &parse_row("????.######..#####. 1,6,5").expect("expected successful parsing")
+            )),
+            2500
+        );
+        assert_eq!(
+            count_arrangements(&unfold_row(
+                &parse_row("?###???????? 3,2,1").expect("expected successful parsing")
+            )),
+            506250
+        );
+        assert_eq!(
+            count_arrangements(
+                &parse_row("#???.#??#??#????# 4,1,5,1,1").expect("expected successful parsing")
+            ),
+            3,
+        );
+        assert_eq!(
+            count_arrangements(&parse_row("# 1,1").expect("expected successful parsing")),
+            0,
         );
     }
 
