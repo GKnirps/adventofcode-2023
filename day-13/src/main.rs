36a2290aed769cd40321a9108b7b01ffb300dc4f
Guido Knips <guido.knips@posteo.de>
2023-12-13 09:40:54 +0100

    Add solution for day 13, part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -13,6 +13,9 @@ fn main() -> Result<(), String> {
     let sum = reflection_line_sum(&patterns);
     println!("The weighted sum of reflection line positions is {sum}");
 
+    let repaired_sum = line_sum_with_repaired_smudge(patterns);
+    println!("The weighted sum of reflection lines without smudges is {repaired_sum}");
+
     Ok(())
 }
 
@@ -20,6 +23,15 @@ fn main() -> Result<(), String> {
 enum Tile {
     Ash,
     Rock,
+}
+
+impl Tile {
+    fn invert(&self) -> Self {
+        match self {
+            Tile::Ash => Tile::Rock,
+            Tile::Rock => Tile::Ash,
+        }
+    }
 }
 
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
@@ -45,13 +57,15 @@ fn reflection_line_sum(patterns: &[Pattern]) -> usize {
     patterns
         .iter()
         .filter_map(|pattern| {
-            v_reflection_line(pattern).or_else(|| h_reflection_line(pattern).map(|y| y * 100))
+            v_reflection_line(pattern)
+                .next()
+                .or_else(|| h_reflection_line(pattern).next().map(|y| y * 100))
         })
         .sum()
 }
 
-fn v_reflection_line(pattern: &Pattern) -> Option<usize> {
-    (1..pattern.width).find(|ref_x| v_reflects_at(pattern, *ref_x))
+fn v_reflection_line(pattern: &Pattern) -> impl Iterator<Item = usize> + '_ {
+    (1..pattern.width).filter(|ref_x| v_reflects_at(pattern, *ref_x))
 }
 
 fn v_reflects_at(pattern: &Pattern, ref_x: usize) -> bool {
@@ -75,8 +89,8 @@ fn v_reflects_at(pattern: &Pattern, ref_x: usize) -> bool {
     matches
 }
 
-fn h_reflection_line(pattern: &Pattern) -> Option<usize> {
-    (1..pattern.height()).find(|ref_y| h_reflects_at(pattern, *ref_y))
+fn h_reflection_line(pattern: &Pattern) -> impl Iterator<Item = usize> + '_ {
+    (1..pattern.height()).filter(|ref_y| h_reflects_at(pattern, *ref_y))
 }
 
 fn h_reflects_at(pattern: &Pattern, ref_y: usize) -> bool {
@@ -98,6 +112,36 @@ fn h_reflects_at(pattern: &Pattern, ref_y: usize) -> bool {
         }
     }
     matches
+}
+
+fn line_sum_with_repaired_smudge(patterns: Vec<Pattern>) -> usize {
+    patterns
+        .into_iter()
+        .filter_map(line_with_repaired_smudge)
+        .sum()
+}
+
+fn line_with_repaired_smudge(mut pattern: Pattern) -> Option<usize> {
+    // brute force again…
+    let orig_line_v = v_reflection_line(&pattern).next();
+    let orig_line_h = h_reflection_line(&pattern).next();
+    for x in 0..pattern.width {
+        for y in 0..pattern.height() {
+            pattern.tiles[x + y * pattern.width] = pattern.tiles[x + y * pattern.width].invert();
+            if let Some(line_v) =
+                v_reflection_line(&pattern).find(|line_v| Some(*line_v) != orig_line_v)
+            {
+                return Some(line_v);
+            }
+            if let Some(line_h) =
+                h_reflection_line(&pattern).find(|line_h| Some(*line_h) != orig_line_h)
+            {
+                return Some(line_h * 100);
+            }
+            pattern.tiles[x + y * pattern.width] = pattern.tiles[x + y * pattern.width].invert();
+        }
+    }
+    None
 }
 
 fn parse(input: &str) -> Result<Vec<Pattern>, String> {
@@ -207,9 +251,21 @@ mod test {
         let pattern = parse_pattern(LARGE_EXAMPLE).expect("expected successful parsing");
 
         // when
-        let line = h_reflection_line(&pattern);
+        let line = h_reflection_line(&pattern).next();
 
         // then
         assert_eq!(line, Some(14));
+    }
+
+    #[test]
+    fn line_sum_with_repaired_smudge_works_for_example() {
+        // given
+        let patterns = parse(EXAMPLE).expect("expected successful parsing");
+
+        // when
+        let sum = line_sum_with_repaired_smudge(patterns);
+
+        // then
+        assert_eq!(sum, 400);
     }
 }
